fdaa42e5f84cf58f720ae3803032f9744a9571cd
MUHAMMAD ADNAN <160736300+adnansheikha@users.noreply.github.com>
2024-03-14 15:47:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// unary operator
+// increment & decrement // ++  --
+//post & pre increment - decrement
+// variable++   ++variable // variable--   --variable 
+//let count:number=0
+//incremnet the count by 1
+//count ++ // count= count +1
+//onsole.log ("count after increment:",count );
+//decrement the count by 1
+//let remainingitem: number=20;
+//remainingitem --;
+//console.log("remaining item after decrement:",remainingitem);
+var x = 8;
+//post increment
+var postincrement = x++;
+console.log("post increment value:", postincrement);
+console.log("value after post increment:", x);
+var preIncrement = ++x;
+console.log("pre Increment value:", preIncrement);
+console.log("value after pre Increment:", x);
+var postdecrement = x--;
+console.log("post decrement value:", postdecrement);
+console.log("value after post decrement:", x);
+var predecrement = --x;
+console.log("pre decrement value:", predecrement);
+console.log("value after pre decrement:", x);
---
+++ main.ts
@@ -0,0 +1,40 @@
+// unary operator
+// increment & decrement // ++  --
+//post & pre increment - decrement
+// variable++   ++variable // variable--   --variable 
+
+//let count:number=0
+
+//incremnet the count by 1
+
+//count ++ // count= count +1
+
+//onsole.log ("count after increment:",count );
+
+//decrement the count by 1
+
+//let remainingitem: number=20;
+
+//remainingitem --;
+
+//console.log("remaining item after decrement:",remainingitem);
+
+let x:number=8
+
+//post increment
+
+let postincrement : number = x++ ;
+console.log("post increment value:",postincrement);
+console.log("value after post increment:",x);
+
+let preIncrement : number = ++x;
+console.log ("pre Increment value:",preIncrement);
+console.log("value after pre Increment:",x);
+
+let postdecrement : number = x-- ;
+console.log("post decrement value:",postdecrement);
+console.log("value after post decrement:",x);
+
+let predecrement : number = --x;
+console.log ("pre decrement value:",predecrement);
+console.log("value after pre decrement:",x);
